62c53ea991b5eca105d111cbfd2f9a45dc514108
Fabrice DUJARDIN <44850811+fabricedujardinportfolio@users.noreply.github.com>
2020-09-02 08:55:44 +0200

    exo 8 debut

--- ExojavaScypt/ExoBase7/Js/main.js
+++ ExojavaScypt/ExoBase7/Js/main.js
@@ -1,10 +1,21 @@
-let myForm = document.getElementById('myForm');
+function myFunction(a,b){
+    let result = (((a * 2) + 5)*50-b)+1766;
+    return result;
+    }
 
+
+
+let myForm = document.getElementById('myForm');
+// console.log(myForm);
 myForm.addEventListener('submit',function(event){
     event.preventDefault();
 
-    let pointure = document.getElementById('pointure');
-    let annee = document.getElementById('annee');
+    let pointure = document.getElementById('pointure').value;
+    // console.log(pointure);
+    let annerNaissance = document.getElementById('annee').value;
+    // console.log(annerNaissance);
 
-    result = pointure.value
-})
+    let result = myFunction(pointure,annerNaissance) ;
+    console.log(result);
+    alert(result);
+});
--- ExojavaScypt/ExoBase7/Js/main1.js
+++
@@ -1,4 +0,0 @@
-function myFunction(a){
-let result = a * 2 + 5;
-
-}
