f4886dc34fd6e0b2a706c1e88f00df0790a8826a
Fabrice DUJARDIN <44850811+fabricedujardinportfolio@users.noreply.github.com>
2020-09-02 10:57:49 +0200

    exercise changer l'image au passage de la souris avec Javascrypt

---
+++ ExojavaScypt/ExoParti2/ExoBase1/Js/main.js
@@ -0,0 +1,15 @@
+// document.getElementById('image1').onmouseover = function(){mouseOver};
+// document.getElementById('image1').onmouseout = function(){mouseOut};
+
+// function mouseOver(){
+// document.getElementById('image1').src = "image1_2.jpg";
+// }
+// function mouseOut(){
+//     document.getElementById('image1').src = "image1.jpg";
+// }
+// imgage1.onMouseOver('image1_2.jpg');
+// document.getElementById('image1').innerHTML = imgage1;
+
+function changeImageOnMouseOver(img){
+    document.getElementById('image1').src="image1_2.jpg";
+    }
