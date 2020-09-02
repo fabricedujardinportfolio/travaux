c21e2314aef3024e0af857a14e8d18075d4566ed
Fabrice DUJARDIN <44850811+fabricedujardinportfolio@users.noreply.github.com>
2020-09-02 09:14:39 +0200

    Cadrant Js corrigé

--- CadranJs/cadran/Js/main.js
+++ CadranJs/cadran/Js/main.js
@@ -2,26 +2,26 @@ let date = new Date();
 let h = date.getHours();
 let m = date.getMinutes();
 let s = date.getSeconds();
-let nh =""
-let nm =""
-let ns =""
+
 function showDate(){
 
     s++;
-    if(s>60){
+    if(s >= 60){
         m++;
         s=0;
-        if(m>60){
-            h++;
-            m=0;
-            s=0;
-            if(h>24){
-                h = 0;
-                m = 0;
-                s = 0;
-            }
-        }
+        
     } 
+    if(m>=60){
+        h++;
+        m=0;
+        
+    }
+    if(h>=24){
+        h = 0;
+    }
+    let ns = s;
+    let nh = h;
+    let nm = m;
 
     if( h < 10 ){ nh = '0' + h; }
     if( m < 10 ){ nm = '0' + m; }
@@ -30,8 +30,8 @@ function showDate(){
     console.log(nh,nm,ns);
 
 
-     let time = h + ':' + m + ':' + s;
-             document.getElementById('horloge').innerHTML = time;
+     let time = nh + ':' + nm + ':' + ns;
+    document.getElementById('horloge').innerText = time;
     //         refresh();
 
 
