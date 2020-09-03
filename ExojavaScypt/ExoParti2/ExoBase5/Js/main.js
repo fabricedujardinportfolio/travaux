2da9114900373db83fcb0b65569d10c646fd6ab3
Fabrice DUJARDIN <44850811+fabricedujardinportfolio@users.noreply.github.com>
2020-09-03 10:42:53 +0200

    Exo 5 5image en Hover

---
+++ ExojavaScypt/ExoParti2/ExoBase5/Js/main.js
@@ -0,0 +1,7 @@
+function changeImageOnMouseOver(img,src){
+    img.src= src;
+    // document.getElementById('image2').src="image2_2.jpg";    
+    // document.getElementById('image3').src="image3_2.jpg";
+    // document.getElementById('image4').src="image4_2.jpg";
+    // document.getElementById('image5').src="image5_2.jpg";
+    }
